81a8cacbe4d3d3255e5f321dd638e68e1ef0a07e
ViniMacacari <vinicius.macacari@edu.unifil.br>
2024-08-31 18:20:49 -0300

    Estilização do login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router'
 import { HomeComponent } from './pages/home/home.component'
 import { InscricaoBarcosComponent } from './pages/inscricao/inscricao-barcos/inscricao-barcos.component'
+import { LoginComponent } from './pages/login/login.component'
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'}, 
     {path: 'home', component: HomeComponent},
     {path: 'inscricao/barcos', component: InscricaoBarcosComponent},
+    {path: 'login', component: LoginComponent},
     {path: '**', redirectTo: 'home'}
 ]
