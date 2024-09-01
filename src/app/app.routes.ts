0d6b4a5bfa5e2423ceba658e19476edb990964c1
ViniMacacari <vinicius.macacari@edu.unifil.br>
2024-09-01 14:37:44 -0300

    Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router'
 import { HomeComponent } from './pages/home/home.component'
 import { InscricaoBarcosComponent } from './pages/inscricao/inscricao-barcos/inscricao-barcos.component'
+import { StatusComponent } from './pages/inscricao/status/status.component'
 import { LoginComponent } from './pages/login/login.component'
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'}, 
     {path: 'home', component: HomeComponent},
     {path: 'inscricao/barcos', component: InscricaoBarcosComponent},
+    {path: 'inscricao/status', component: StatusComponent},
     {path: 'login', component: LoginComponent},
     {path: '**', redirectTo: 'home'}
 ]
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core'
+import { Component, ViewChild } from '@angular/core'
+import { HttpClient, HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
 import { FormatInputDirective } from '../../directives/format-input.directive'
+import { VariaveisAmbiente } from '../../config/ambiente'
+import { ModalComponent } from "../../componentes/modal/modal.component"
+import { ModalResponseService } from '../../services/modal/modal-response.service'
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormatInputDirective],
+  imports: [FormatInputDirective, HttpClientModule, FormsModule, ModalComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor(
+    private http: HttpClient,
+    private responseService: ModalResponseService
+  ) { }
 
+  @ViewChild('Modal') Modal!: ModalComponent
+
+  url = `${VariaveisAmbiente.urlApi}/usuarios/login`
+
+  nomeEquipe = ''
+  cpf = ''
+
+  login(): void {
+    this.Modal.openModal('#ffd042', 'Aguarde...', 'Estamos confirmando seus dados!', 'fa-spinner fa-spin', false, '', false, '', '', '')
+
+    const body = {
+      equipe: this.nomeEquipe,
+      cpf: this.cpf
+    }
+
+    this.http.post(this.url, body).subscribe(
+      (res: any) => {
+        localStorage.setItem('ccf', res.token)
+        window.location.href = '/inscricao/status'
+      },
+      (err) => {
+        this.Modal.openModal('#FF0000', 'Erro', 'Não encontramos nenhum usuário inscrito com esses dados.', 'fa-exclamation-triangle', true, 'Ok', false, '', '', '')
+      }
+    )
+  }
 }
