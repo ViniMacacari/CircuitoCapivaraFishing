6147019c4052335f39c46786c6819f8e184a0211
ViniMacacari <macacarivinicius@gmail.com.br>
2024-11-01 13:01:37 -0300

    Não mostrar menu no admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,21 @@
 import { Routes } from '@angular/router'
+
 import { HomeComponent } from './pages/home/home.component'
 import { InscricaoBarcosComponent } from './pages/inscricao/inscricao-barcos/inscricao-barcos.component'
+
 import { StatusComponent } from './pages/inscricao/status/status.component'
 import { LoginComponent } from './pages/login/login.component'
 
+import { AdminComponent } from './pages/admin/admin.component'
+
 export const routes: Routes = [
-    {path: '', redirectTo: 'home', pathMatch: 'full'}, 
-    {path: 'home', component: HomeComponent},
-    {path: 'inscricao/barcos', component: InscricaoBarcosComponent},
-    {path: 'inscricao/status', component: StatusComponent},
-    {path: 'login', component: LoginComponent},
-    {path: '**', redirectTo: 'home'}
+    { path: '', pathMatch: 'full', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'inscricao/barcos', component: InscricaoBarcosComponent },
+    { path: 'inscricao/status', component: StatusComponent },
+    { path: 'login', component: LoginComponent },
+
+    { path: 'admin', component: AdminComponent },
+
+    { path: '**', redirectTo: 'home' }
 ]
--- src/app/componentes/menu/menu.component.ts
+++ src/app/componentes/menu/menu.component.ts
@@ -1,5 +1,6 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { Router, NavigationEnd } from '@angular/router'
 
 @Component({
   selector: 'app-menu',
@@ -9,5 +10,21 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
+  constructor(
+    private router: Router
+  ) { }
 
+  mostrar: boolean = true
+
+  ngAfterViewInit(): void {
+    this.esconderMenu()
+  }
+
+  esconderMenu(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.mostrar = !event.url.includes('/admin')
+      }
+    })
+  }
 }
